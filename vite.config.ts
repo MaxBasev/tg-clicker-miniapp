5b472f76e8ab5b66cafedb44bad40d47c3caf75c
Max Basev <maxbasev@gmail.com>
2026-01-07 20:30:21 +0600

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    build: {
-        outDir: 'build',
-    },
+
     test: {
         globals: true,
         environment: 'jsdom',
